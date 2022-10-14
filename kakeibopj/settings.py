1b6df22f4bfd030fb536ddcc4a7d8478ad6e6c42
kengo78 <fulken35@gmail.com>
2022-10-15 00:17:34 +0900

    accounts app 作成

--- kakeibopj/settings.py
+++ kakeibopj/settings.py
@@ -15,7 +15,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG")
 
 # ALLOWED_HOSTSを.envから取得
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS=['localhost' ,'127.0.0.1']
 
 
 # Application definition
