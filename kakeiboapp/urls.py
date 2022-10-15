39e611c0093a965b23f2a39c481d2d1d2383165f
kengo78 <fulken35@gmail.com>
2022-10-15 04:24:05 +0000

    カスタムユーザモデルのUserが使えていないのでエラー発生中

--- kakeiboapp/urls.py
+++ kakeiboapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = "kakeiboapp"
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('home/', views.HomeView.as_view(), name="home"),
+    path('signup/', views.SignUpView.as_view(), name="signup"),
 ]
--- kakeiboapp/views.py
+++ kakeiboapp/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.http.response import HttpResponseRedirect
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth import login
+from django.urls import reverse_lazy
+
+from .forms import SignUpForm
 
 class IndexView(TemplateView):
     template_name = "kakeiboapp/index.html"
+    
+class HomeView(TemplateView):
+    template_name = "kakeiboapp/home.html"
+    
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    template_name = "kakeiboapp/signup.html"
+    success_url = reverse_lazy("kakeiboapp:home")
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        self.object = user
+        return HttpResponseRedirect(self.get_success_url())
